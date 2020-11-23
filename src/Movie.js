0e424de2369d8c8ca96e6fd2404a314611002e98
NetMD <cylee0510@gmail.com>
2020-11-23 23:56:14 +0900

    complete

---
+++ src/Movie.js
@@ -0,0 +1,32 @@
+import React from "react";
+import propTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({id, year, title, summary, poster, genres}) {
+	return <div class="movie">
+		<img src={poster} alt={title} title={title} />
+		<div class="movive__data">
+			<h3 class="movie__title">{title}</h3>
+			<h5 class="movie__year">{year}</h5>
+			<ul class="genres">
+				{genres.map((genre, index) => (
+						<li key={index} class="genres__genre">{genre}</li>
+					))
+				}
+			</ul>
+			<p class="movie__summary">{summary.slice(0, 180)}</p>
+		</div>
+	</div>;
+}
+
+// eslint-disable-next-line react/no-typos
+Movie.propTypes = {
+	id : propTypes.number.isRequired,
+	year : propTypes.number.isRequired,
+	title : propTypes.string.isRequired,
+	summary : propTypes.string.isRequired,
+	poster : propTypes.string.isRequired,
+	genres : propTypes.arrayOf(propTypes.string).isRequired
+}
+
+export default Movie;
